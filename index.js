e59f9c701df6e220d19a92398c6382126b213d88
bcharlotin1 <biancacharlotin1@gmail.com>
2021-09-15 18:07:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 
+
+function  superbowlWin(array) {
+
+   const winner =  array.find(function(element){
+
+        return element.result === "W"    
+
+    })
+    if (winner){
+        return winner.year
+    }
+}
+
+// const record = [
+//     {year: "2018", result: "L"},
+//     {year: "2017", result: "W"},
+//     {year: "2016", result: "N/A"}
+//     //...
+//   ]
